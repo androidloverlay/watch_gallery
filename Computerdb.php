3afa90e721a373e93a87b20d1d231ab716c6185f
androidloverlay <androidloverlay999@gmail.com>
2017-03-03 16:57:12 +0630

    Update Computerdb.php

--- Computerdb.php
+++ Computerdb.php
@@ -1,6 +1,14 @@
 <?php
 
-$conn=mysql_connect("localhost","root","","") or die("Can't connect to database!");
-mysql_select_db("thewatchgallery");
+/* $conn=mysql_connect("localhost","root","","") or die("Can't connect to database!");
+ mysql_select_db("thewatchgallery"); */
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn=mysql_connect($server, $username, $password, $db) or die("Can't connect to database!");
+/* $conn = new mysqli($server, $username, $password, $db); */
+mysql_select_db("thewatchgallery");
 ?>
